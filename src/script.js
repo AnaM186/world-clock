ea8e4a54bec6e0b4825f41ef5a34d583f7a4992b
AnaM186 <annamoskalenko1501@gmail.com>
2025-06-09 14:32:55 +0100

    Current time and date of each selected city

--- src/script.js
+++ src/script.js
@@ -1,22 +1,52 @@
 function displayTime() {
   let vancouverDateElement = document.querySelector("#vancouver-date");
-  vancouverDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  if (vancouverDateElement) {
+    vancouverDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  }
   let vancouverTimeElement = document.querySelector("#vancouver-time");
-  vancouverTimeElement.innerHTML = moment()
-    .tz("America/Vancouver")
-    .format("h:mm:ss [<small>]A[</small>]");
+  if (vancouverTimeElement) {
+    vancouverTimeElement.innerHTML = moment()
+      .tz("America/Vancouver")
+      .format("h:mm:ss [<small>]A[</small>]");
+  }
   let barcelonaDateElement = document.querySelector("#barcelona-date");
-  barcelonaDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  if (barcelonaDateElement) {
+    barcelonaDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  }
   let barcelonaTimeElement = document.querySelector("#barcelona-time");
-  barcelonaTimeElement.innerHTML = moment()
-    .tz("Europe/Madrid")
-    .format("h:mm:ss [<small>]A[</small>]");
+  if (barcelonaTimeElement) {
+    barcelonaTimeElement.innerHTML = moment()
+      .tz("Europe/Madrid")
+      .format("h:mm:ss [<small>]A[</small>]");
+  }
   let jakartaDateElement = document.querySelector("#jakarta-date");
-  jakartaDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  if (jakartaDateElement) {
+    jakartaDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  }
   let jakartaTimeElement = document.querySelector("#jakarta-time");
-  jakartaTimeElement.innerHTML = moment()
-    .tz("Asia/Jakarta")
-    .format("h:mm:ss [<small>]A[</small>]");
+  if (jakartaTimeElement) {
+    jakartaTimeElement.innerHTML = moment()
+      .tz("Asia/Jakarta")
+      .format("h:mm:ss [<small>]A[</small>]");
+  }
 }
 displayTime();
 setInterval(displayTime, 1000);
+
+function displayCity(event) {
+  let chosenCity = event.target.value;
+  let city = chosenCity.split("/")[1];
+  let date = moment().format("MMMM Do YYYY");
+  let time = moment().tz(chosenCity).format("h:mm:ss [<small>]A[</small>]");
+  let cityContainer = document.querySelector("#city-container");
+  cityContainer.innerHTML = `<div class="city">
+          <div>
+            <h2>${city}</h2>
+            <div class="date">${date}</div>
+          </div>
+          <div class="time">${time}</div>
+        </div>`;
+}
+
+let selectCityElement = document.querySelector("#select-city");
+selectCityElement.addEventListener("change", displayCity);
